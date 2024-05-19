8eb9bf8ff3c12566fb7808d93d16026d4d3ed81c
Uzma-Azam21 <uzmaazam241@gmail.com>
2024-05-19 07:05:03 -0700

    Add simple calculator project

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+// Simple Command Line Calculator
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
+
+  {
+    message: "Select one of operators to perform operations",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// CONDTIONAL STATEMENT
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  ("Please select correct operator");
+}
